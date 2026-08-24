6f42b729cb8568e5ef3b367645a36e464159500c
vunhatthong10122007-rgb <vunhatthong10122007@gmail.com>
2026-08-24 07:04:33 +0700

    Delete app directory

--- app/dependencies/__init__.py
+++
@@ -1,3 +0,0 @@
-from app.db.database import get_db
-
-__all__ = ["get_db"]
--- app/routers/__init__.py
+++
@@ -1,3 +0,0 @@
-from .health import router as Health_check
-from .auth import router as Auth_router
-from .users import router as Users_router
